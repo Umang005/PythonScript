9122cb2b9954fa3744f5178bdb171b2e175c35bc
Umang Srivastava <60014719+Umang005@users.noreply.github.com>
2021-09-20 03:00:25 +0530

    Update PrizeToToppers.py

--- PrizeToToppers.py
+++ PrizeToToppers.py
@@ -5,9 +5,9 @@ import pandas as pd
 df = pd.read_csv(r"C:\Users\Ashish\Documents\USE CASE\TopperList.csv")
 for i in range(0,3):
     if 1<=df.loc[i,"Rank"]<2:
-      print(str(df.loc[i,"Student Name"])+" ="+ "Gold")
+      print(str(df.loc[i,"Student Name"])+" = "+ "Gold")
     elif 2<=df.loc[i,"Rank"]<3:
-      print(str(df.loc[i,"Student Name"])+" ="+ "Silver")
+      print(str(df.loc[i,"Student Name"])+" = "+ "Silver")
     elif 3<=df.loc[i,"Rank"]<4:
-      print(str(df.loc[i,"Student Name"])+" ="+ "Bronze")
+      print(str(df.loc[i,"Student Name"])+" = "+ "Bronze")
 
