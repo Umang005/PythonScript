3fc016462c342ba582dcd7b5456392e526c2538d
Umang Srivastava <60014719+Umang005@users.noreply.github.com>
2021-09-22 01:36:38 +0530

    Update PrizeToToppers.py

--- PrizeToToppers.py
+++ PrizeToToppers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv(r"C:\Users\Ashish\Documents\USE CASE\Student.csv")
 for i in range(0,3):
@@ -9,3 +10,11 @@ for i in range(0,3):
     elif 3<=df.loc[i,"Rank"]<4:
       print(str(df.loc[i,"Student Name"])+" ="+ "Bronze")
 
+
+fig = plt.figure(figsize = (10, 5))
+plt.bar(df["Student Name"],df["Rank"],color='maroon',width=0.4)
+
+plt.xlabel("Students Name")
+plt.ylabel("Rank Obtained")
+plt.title("Students Ranking ")
+plt.show()
