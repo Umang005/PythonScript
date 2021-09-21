8e1dc11e69456ceba5b5b06413f097a4e040fea0
Umang Srivastava <60014719+Umang005@users.noreply.github.com>
2021-09-22 01:01:24 +0530

    Update LoadExcel.py

--- LoadExcel.py
+++ LoadExcel.py
@@ -1,6 +1,16 @@
 import pandas as pd
-
+import sys
+import matplotlib.pyplot as plt
 df = pd.read_csv(r"C:\Users\Ashish\Documents\USE CASE\Student.csv")
 df["Percentage"]=(df["Maths"]+df["Physics"]+df["Chemistry"])*0.3
 print(df)
 df.to_csv(r"C:\Users\Ashish\Documents\USE CASE\Student.csv",index=False)
+name=sys.argv[1]
+fig = plt.figure(figsize = (10, 5))
+plt.bar(df["Student Name"],df[name],color='maroon',width=0.4)
+
+plt.xlabel("Courses offered")
+plt.ylabel("No. of students enrolled")
+plt.title("Students enrolled in different courses")
+plt.show()
+
